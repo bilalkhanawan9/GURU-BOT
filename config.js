aba85d3dd3cb12e2f14728725180a08061c1d3fa
bilalkhanawan9 <143666955+bilalkhanawan9@users.noreply.github.com>
2024-01-29 03:39:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['92313382049', 'Awan Ai Bot ', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
